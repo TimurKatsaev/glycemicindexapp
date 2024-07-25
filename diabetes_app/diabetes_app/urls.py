a63de2d5e0659b611a5b9971da4d4792b8a90021
Timur Katsaev <martyshka31122016@gmail.com>
2024-07-25 22:47:25 +0600

    Finished setting up the prject

--- diabetes_app/diabetes_app/urls.py
+++ diabetes_app/diabetes_app/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from glycemic_index_app.views import *
+from django.urls import path, include
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('gi-webapp/', include('glycemic_index_app.urls')),
 ]
