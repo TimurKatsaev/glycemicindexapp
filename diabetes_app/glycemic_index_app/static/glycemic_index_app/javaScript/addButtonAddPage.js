ae5acad9e208ea48a3f76efd10049f4ac4736d2e
Timur Katsaev <martyshka31122016@gmail.com>
2024-08-19 18:43:51 +0600

    Added gi calculator js

--- diabetes_app/glycemic_index_app/static/glycemic_index_app/javaScript/addButtonAddPage.js
+++ diabetes_app/glycemic_index_app/static/glycemic_index_app/javaScript/addButtonAddPage.js
@@ -7,6 +7,7 @@ function addFn() {
     iFeild.setAttribute("type", "text");
     iFeild.setAttribute("placeholder", "Число от 1 до 100");
     iFeild.classList.add("form-GI");
+    iFeild.setAttribute("name", "form-GI");
 
     button.classList.add("minus-button");
     button.setAttribute("onclick", "delNode(this)");
---
+++ diabetes_app/glycemic_index_app/static/glycemic_index_app/javaScript/giCalculator.js
@@ -0,0 +1,23 @@
+const form = document.getElementById('add-form');
+
+form.addEventListener('submit', function (event) {
+    event.preventDefault(); // Предотвращаем стандартное действие отправки формы
+    let giInputList = []
+
+    const giInputs = document.getElementsByClassName("form-GI");
+
+    Array.prototype.forEach.call(giInputs, el => {
+        giInputList.push(el.value);
+    })
+
+    // Выполняем вычисления (например, складываем числа)
+    const sum = giInputList.reduce((a, b) => parseInt(a) + parseInt(b), 0);
+    const avg = (sum / giInputList.length) || 0;
+
+    // Заполняем результат в отдельное поле
+    document.getElementById('avg-gi').value = avg;
+    document.getElementById('avg-rcg').value = avg / 1000;
+
+    // После вычислений отправляем форму вручную
+    form.submit(); // Отправляем форму
+});
